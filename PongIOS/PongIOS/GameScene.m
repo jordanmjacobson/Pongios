4aa1cc5ecbf3c66367a8d942669ddbfa739faa1a
Jordan Jacobson <jjacob44@binghamton.edu>
2019-10-07 09:57:39 -0400

    commented out spinny object stuff

--- PongIOS/PongIOS/GameScene.m
+++ PongIOS/PongIOS/GameScene.m
@@ -9,8 +9,8 @@
 #import "GameScene.h"
 
 @implementation GameScene {
-    SKShapeNode *_spinnyNode;
-    SKLabelNode *_label;
+    //SKShapeNode *_spinnyNode;
+    //SKLabelNode *_label;
     SKSpriteNode *_bar;
 }
 
@@ -18,49 +18,49 @@
     // Setup your scene here
     
     // Get label node from scene and store it for use later
-    _label = (SKLabelNode *)[self childNodeWithName:@"//helloLabel"];
+   // _label = (SKLabelNode *)[self childNodeWithName:@"//helloLabel"];
     _bar = (SKSpriteNode *)[self childNodeWithName:@"//bar"];
     
-    _label.alpha = 0.0;
-    [_label runAction:[SKAction fadeInWithDuration:2.0]];
+    //_label.alpha = 0.0;
+    //[_label runAction:[SKAction fadeInWithDuration:2.0]];
     
-    CGFloat w = (self.size.width + self.size.height) * 0.05;
+    //CGFloat w = (self.size.width + self.size.height) * 0.05;
     
     // Create shape node to use during mouse interaction
-    _spinnyNode = [SKShapeNode shapeNodeWithRectOfSize:CGSizeMake(w, w) cornerRadius:w * 0.3];
-    _spinnyNode.lineWidth = 2.5;
+   // _spinnyNode = [SKShapeNode shapeNodeWithRectOfSize:CGSizeMake(w, w) cornerRadius:w * 0.3];
+    //_spinnyNode.lineWidth = 2.5;
     
-    [_spinnyNode runAction:[SKAction repeatActionForever:[SKAction rotateByAngle:M_PI duration:1]]];
-    [_spinnyNode runAction:[SKAction sequence:@[
-                                                [SKAction waitForDuration:0.5],
-                                                [SKAction fadeOutWithDuration:0.5],
-                                                [SKAction removeFromParent],
-                                                ]]];
+    //[_spinnyNode runAction:[SKAction repeatActionForever:[SKAction rotateByAngle:M_PI duration:1]]];
+   // [_spinnyNode runAction:[SKAction sequence:@[
+                                                //[SKAction waitForDuration:0.5],
+                                                //[SKAction fadeOutWithDuration:0.5],
+                                                //[SKAction removeFromParent],
+                                                //]]];
 }
 
 
 - (void)touchDownAtPoint:(CGPoint)pos {
-    SKShapeNode *n = [_spinnyNode copy];
-    n.position = pos;
-    n.strokeColor = [SKColor greenColor];
-    [self addChild:n];
+    //SKShapeNode *n = [_spinnyNode copy];
+    //n.position = pos;
+    //n.strokeColor = [SKColor greenColor];
+    //[self addChild:n];
     
     pos.y = 50;
     [_bar setPosition:pos];
 }
 
 - (void)touchMovedToPoint:(CGPoint)pos {
-    SKShapeNode *n = [_spinnyNode copy];
-    n.position = pos;
-    n.strokeColor = [SKColor blueColor];
-    [self addChild:n];
+    //SKShapeNode *n = [_spinnyNode copy];
+    //n.position = pos;
+   // n.strokeColor = [SKColor blueColor];
+    //[self addChild:n];
 }
 
 - (void)touchUpAtPoint:(CGPoint)pos {
-    SKShapeNode *n = [_spinnyNode copy];
-    n.position = pos;
-    n.strokeColor = [SKColor redColor];
-    [self addChild:n];
+    //SKShapeNode *n = [_spinnyNode copy];
+    //n.position = pos;
+    //n.strokeColor = [SKColor redColor];
+    //[self addChild:n];
     
     pos.y = 50;
     [_bar setPosition:pos];
@@ -68,7 +68,7 @@
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     // Run 'Pulse' action from 'Actions.sks'
-    [_label runAction:[SKAction actionNamed:@"Pulse"] withKey:@"fadeInOut"];
+    //[_label runAction:[SKAction actionNamed:@"Pulse"] withKey:@"fadeInOut"];
     //[_bar runAction:[SKAction actionNamed:@"Pulse"] withKey:@"fadeInOut"];
     for (UITouch *t in touches) {[self touchDownAtPoint:[t locationInNode:self]];}
 }
